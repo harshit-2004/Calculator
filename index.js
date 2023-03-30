37dd75f124d7e2adc45a8816f05d5cc5f5f00b25
Harshit <96365691+harshit-2004@users.noreply.github.com>
2023-03-30 23:15:01 +0530

    Update index.js
    adding keystrokes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var num1 = "",num2="",operator="";
 var display = document.getElementById("display");
 var buttons = document.getElementsByClassName("button");
-for(var i = 0;(operator==0);i++)
+
+for(var i = 0;i<buttons.length;i++)
 {
     buttons[i].addEventListener('click',function(){
         var value = this.getAttribute('data-value');
@@ -35,8 +36,6 @@ for(var i = 0;(operator==0);i++)
             if(num1=='')
             {
                 if(operator!='')
-                // num2=="";
-                // else
                 operator="";
             }else{
                 num1="";
@@ -50,5 +49,67 @@ for(var i = 0;(operator==0);i++)
     })
 }
 
-var t = document;
-console.log(t);
+    document.addEventListener('keypress', (event) => {
+        var value = event.key;
+        // if(value == 'Backspace')
+        // alert("This is not ");
+        console.log(value);
+        if(value=='+'||value=='-'||value=="*"||value=='/')
+        {
+            // console.log(num1+" "+operator+" "+num2);
+            num2 = num1;
+            operator=value;
+            num1 = "";
+            display.innerText+=value;
+        }
+        else if(value =='%'){
+            num1 = eval(num1/100);
+            display.innerText = num1;
+        }else if(value == '='||value == 'Enter')
+        {
+            // console.log("result " + num1 +" "+operator+" "+num2);
+            num1 = eval(num2+operator+num1);
+            display.innerText=num1;
+            num2 = "";
+            operator="";
+        }
+        else if(value=="sign")
+        {
+            num1 = (-1)*num1;
+            display.innerText=num2+operator+num1;
+        }
+        else if(value == 'Backspace'||value=='delete'||value=='Delete'){
+            if(num1==''&&operator=='')
+            num2="";
+            if(num1=='')
+            {
+                if(operator!='')
+                operator="";
+            }else{
+                num1="";
+            }
+            // console.log(num2);
+            display.innerText=num2+operator+num1;
+        }else if(value=='1'||value=='2'||value=='3'||value=='4'||value=='5'||value=='6'||value=='7'||value=='8'||value=='9'||value=='0'){
+            num1+=value;
+            display.innerText=num2+operator+num1;
+        }
+    },false);
+
+    // document.addEventListener('keypress',(event)=>{
+    //     var value = event.key;
+    //     if(value=='Backspace')
+    //     {
+    //         if(num1==''&&operator=='')
+    //         num2="";
+    //         if(num1=='')
+    //         {
+    //             if(operator!='')
+    //             operator="";
+    //         }else{
+    //             num1="";
+    //         }
+    //         // console.log(num2);
+    //         display.innerText=num2+operator+num1;
+    //     }
+    // })
